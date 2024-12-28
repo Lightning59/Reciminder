efe90fa01aec4af10649336e33a9183ca923048b
Evan McCaleb <evanmccaleb@gmail.com>
2024-12-28 16:17:38 -0500

    Got same machine postgresworking.

--- Django/Reciminder/settings.py
+++ Django/Reciminder/settings.py
@@ -86,13 +86,31 @@ WSGI_APPLICATION = 'Reciminder.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+db_mode=os.environ.get('DJANGO_DB_MODE', '')
+if db_mode.lower() == "dev":
+    print('sqllite')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DB_NAME=os.environ.get('DJANGO_DB', '')
+    DB_USER=os.environ.get('DJANGO_DB_USER', '')
+    DB_PASSWORD=os.environ.get('DJANGO_DB_PASSWORD', '')
+    DB_HOST = os.environ.get('DJANGO_DB_HOST', '')
+    DB_PORT = os.environ.get('DJANGO_DB_PORT', '')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': DB_NAME,
+            'USER': DB_USER,
+            'PASSWORD': DB_PASSWORD,
+            'HOST': DB_HOST,
+            'PORT': DB_PORT,
+        }
+    }
 
 
 # Password validation
