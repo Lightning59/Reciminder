23dd00f13c2d244bebfb3d16a6ac2f157e8914be
Evan McCaleb <evanmccaleb@gmail.com>
2024-12-28 00:40:32 -0500

    Setup first stage of docker integration.

--- Django/Reciminder/settings.py
+++ Django/Reciminder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-(e5vnps6rlgc@-=to3ic8cmgt7fa_rtu11@ctv6wt56l^j&9x+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.2.214', 'recipe.test']
 
 
 # Application definition
