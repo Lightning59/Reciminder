7ac01197c0a7c72d898d56e9aff65625d97ab87f
Evan McCaleb <evanmccaleb@gmail.com>
2024-12-31 19:25:02 -0500

    Have feature #5 now working.

--- Django/landingpages/views.py
+++ Django/landingpages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from recipe.models import Recipe
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
@@ -11,5 +12,23 @@ def index(request):
 def logged_in_home(request):
     user = request.user
     all_recipes = Recipe.objects.filter(deleted_by_user=False).all()
-    context = {'user': user, 'recipes': all_recipes}
+    # My Current guess for a good deafult results for page is 30 - anything lower is still experimentation
+    this_page_recipes, total_pages = paginate_recipes(request, all_recipes, 4)
+    page_range=list(range(1, total_pages+1))
+    context = {'user': user, 'recipes': this_page_recipes, 'page_range': page_range, 'total_pages': total_pages}
     return render(request, 'logged_in_temp.html', context)
+
+
+def paginate_recipes(request, recipes, num_results_per_page):
+    curr_page = request.GET.get('page')
+    paginator = Paginator(recipes, num_results_per_page)
+    total_pages = paginator.num_pages
+    try:
+        page_recipes = paginator.page(curr_page)
+    except PageNotAnInteger:
+        curr_page = 1
+        page_recipes = paginator.page(curr_page)
+    except EmptyPage:
+        curr_page = total_pages
+        page_recipes = paginator.page(curr_page)
+    return page_recipes, total_pages
