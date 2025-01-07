7cae25ebea2cb1c6ef075ead0f7151acce9458bd
Evan McCaleb <evanmccaleb@gmail.com>
2025-01-06 22:52:49 -0500

    Wrote First two tests.

--- Django/landingpages/tests.py
+++ Django/landingpages/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+
+class LandingPageViewTests(TestCase):
+    def test_call_index_directly(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'landing_page.html')
+        self.assertContains(response, 'Project is still in work.</p>')
+
+    def test_call_index_by_reverse(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'landing_page.html')
+        self.assertContains(response, 'Project is still in work.</p>')
